9e91e3d250ca4ef02d0cd0c2223d5eadb5976aa7
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-06-17 12:23:09 +0100

    added deprecation function shorthand, marked, many template functions as deprecated

--- includes/govpack-functions-template.php
+++ includes/govpack-functions-template.php
@@ -33,49 +33,69 @@ if ( ! function_exists( 'gp_get_available_widths' ) ) {
 				'maxWidth' => 'none',
 			],
 		];
-	}
  }
+}
 
- function gp_classnames(string|array $classnames = "", array $candidates = [] ){
+if(!function_exists("gp_classnames")){
+	function gp_classnames(string|array $classnames = "", array $candidates = [] ){
 
-	if(is_array(($classnames))){
-		$classnames = trim(join(" ", $classnames));
-	}
-
-	$selection = [];
-	foreach($candidates as $key => $value){
-		if(is_int($key)){
-			$selection[] = $value;
-			continue;
+		if(is_array(($classnames))){
+			$classnames = trim(join(" ", $classnames));
 		}
 
-		if($value === true){
-			$selection[] = $key;
-			continue;
+		$selection = [];
+		foreach($candidates as $key => $value){
+			if(is_int($key)){
+				$selection[] = $value;
+				continue;
+			}
+
+			if($value === true){
+				$selection[] = $key;
+				continue;
+			}
+			
 		}
 		
+
+		return trim($classnames . " ". join(
+			" ",
+			$selection
+		)); 
 	}
-	
-
-	return trim($classnames . " ". join(
-		" ",
-		$selection
-	)); 
  }
 
- function gp_line_attributes($line, $attributes){
-	
-	$elm_attributes = [
-		"id" => esc_attr(sprintf("govpack-profile-block-%s", $line["key"])),
-		"class" =>  esc_attr(gp_classnames("wp-block-govpack-profile__line", [
-			"wp-block-govpack-profile__line--" . $line["key"],
-			"govpack-line",
-			"govpack-line--labels-" . ($attributes["labelsAbove"] ? "above" : "beside"),
-		]))
-	];
 
-	return gp_normalise_html_element_args($elm_attributes);
+if(!function_exists("gp_normalise_html_element_args")){
+	function gp_normalise_html_element_args($elm_attributes){
+
+		$normalized_attributes = array();
+		foreach ( $elm_attributes as $key => $value ) {
+			$normalized_attributes[] = $key . '="' . esc_attr( $value ) . '"';
+		}
+
+		$elm_attributes = implode( ' ', $normalized_attributes );
+
+		return trim($elm_attributes);
+	}
  }
+
+
+if(!function_exists("gp_line_attributes")){
+	function gp_line_attributes($line, $attributes){
+		
+		$elm_attributes = [
+			"id" => esc_attr(sprintf("govpack-profile-block-%s", $line["key"])),
+			"class" =>  esc_attr(gp_classnames("wp-block-govpack-profile__line", [
+				"wp-block-govpack-profile__line--" . $line["key"],
+				"govpack-line",
+				"govpack-line--labels-" . ($attributes["labelsAbove"] ? "above" : "beside"),
+			]))
+		];
+
+		return gp_normalise_html_element_args($elm_attributes);
+	}
+}
 
  function gp_normalise_html_element_args($elm_attributes){
 
@@ -123,42 +143,6 @@ if(!function_exists("gp_style_attribute_generator")){
 	}
 }
 
-
-/**
- * Utility Function that Outputs a row
- * 
- * @param string  $id The html ID to output.
- * @param string  $value The value to output.
- * @param boolean $display Override to control if this row will output.
- */
-/*
-function gp_row( $id, $value, $display ) {
-
-		if ( ! $display ) {
-			return null;
-		}
-
-		if ( ! $value ) {
-			return null;
-		}
-
-		// No escaping here. $value here needs to handle HTML beyond what wp_kses can realistically handle. Escaping should be done before passing to this function.
-		echo '<div id="govpack-profile-block-' . $id . '" class="wp-block-govpack-profile__line wp-block-govpack-profile__line--' . $id . '">' . $value . '</div>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	}
-}
-*/
-
-/**
- * Utility Function that Outputs a link to a profile
- * 
- * @param string  $url The url to link to.
- * @param boolean $title Name of the profile to link to eg More About {$title}.
- */
-/*
-function gp_link( $url, $title ) {
-	return '<a href=' . esc_url( $url ) . '>More About ' . esc_html( $title ) . '</a>';
-}
-*/
 
 
 /**
@@ -212,246 +196,8 @@ if ( ! function_exists( 'gp_websites' ) ) {
 	}
 }
 
-/**
- * Utility Function that Outputs a Profiles Social and Email.
- * 
- * @param array $profile_data Data about the profile.
- * @param array $attributes Attributes from the Block.
- */
-if ( ! function_exists( 'gp_contacts' ) ) {
-	function gp_contacts( $profile_data, $attributes ) {
-
-		$icons = gp_get_icons();
-	
-		$icon = '<span class="wp-block-govpack-profile__contact__icon wp-block-govpack-profile__contact__icon--{%s}">%s</span>';
-
-		if ( $attributes['showEmail'] && $profile_data['email'] ) {
-			$email_icon = sprintf( $icon, 'email', gp_get_icon( 'email' ) );
-			$classes    = [
-				'wp-block-govpack-profile__contact--hide-label',
-			];
-
-			$classes = esc_attr( join( ' ', $classes ) );
-
-			$email = "<li class=\"wp-block-govpack-profile__contact {$classes}\">
-				<a href=\"mailto:{$profile_data['email']}\" class=\"wp-block-govpack-profile__contact__link\">
-					{$email_icon}
-					<span class=\"wp-block-govpack-profile__contact__label\">Email</span>
-				</a>
-			</li>";
-
-		}
-	
-
-		$social = '';
-		if ( $attributes['showSocial'] ) {
-
-			$services = [ 'facebook', 'x', 'linkedin', 'instagram' ];
-
-
-			foreach ( $services as $service ) {
-				if ( ! isset( $profile_data[ $service ] ) || ! $profile_data[ $service ] ) {
-					continue;
-				}
-
-				$classes = [
-					'wp-block-govpack-profile__contact',
-					'wp-block-govpack-profile__contact--hide-label',
-					"wp-block-govpack-profile__contact--{$service}",
-				];
-		
-				$classes = esc_attr( join( ' ', $classes ) );
-
-
-				$contact_icon = sprintf( $icon, $service, gp_get_icons( $service ) );
-				$social      .=  
-				"<li class=\"{$classes} \">
-					<a href=\"{$profile_data[$service]}\" class=\"wp-block-govpack-profile__contact__link\">
-						{$contact_icon}
-						<span class=\"wp-block-govpack-profile__contact__label\">{$service}</span>
-					</a>
-				</li>";
-
-			}
-		}
-
-
-		return sprintf(
-			'<div class="wp-block-govpack-profile__contacts">
-					<ul>
-						%s
-						%s
-					</ul>
-				</div>',
-			$email ?? '',
-			$social ?? '',
-		);
-	}
-}
-
-
-if ( ! function_exists( 'gp_get_profile_links' ) ) {
-	function gp_get_profile_links( $profile_data ) {
-		
-		if ( ! isset( $profile_data['links'] ) ) {
-			return [];
-		}
-
-		if ( empty( $profile_data['links'] ) ) {
-			return [];
-		}
-
-		
-		$links = apply_filters( 'govpack_profile_links', $profile_data['links'] ?? [], $profile_data['id'], $profile_data );
-		foreach ( $links as &$link ) {
-
-			$link = apply_filters( 'govpack_profile_link', $link, $profile_data['id'], $profile_data );
-
-			$link_attrs = array_filter(
-				$link,
-				function ( $value, $key ) {
-					if ( ( $value === null ) || ( $value === '' ) ) {
-						return false;
-					}
-
-					if ( ( $key === 'text' ) || ( $key === 'meta' ) ) {
-						return false;
-					}
-				
-					if ( is_array( $value ) && ( empty( $value ) ) ) {
-						return false;
-					}
-
-					return true;
-				},
-				ARRAY_FILTER_USE_BOTH
-			);
-
-			$link['src'] = sprintf( '<a %s>%s</a>', gp_normalise_html_element_args( $link_attrs ), $link['text'] );
-			
-		}
-
-		return $links;
-	}
-}
-
-
-if ( ! function_exists( 'gp_the_profile_links' ) ) {
-
-	function gp_the_profile_links( $profile_data, $attributes ) {
-
-		$links = gp_get_profile_links( $profile_data );
-
-		foreach ( $links as $key => &$link ) {
-			$link['show'] = gp_should_show_link( $key, $attributes );
-		}   
-
-		$links = array_filter(
-			$links,
-			function ( $link ) {
-				return $link['show'];
-			},
-			ARRAY_FILTER_USE_BOTH 
-		);
-
-		if ( count( $links ) <= 0 ) {
-			return '';
-		}
-
-
-		ob_start();
-		?>
-		<div class="wp-block-govpack-profile__comms">
-			<div class="wp-block-govpack-profile__label">Links:</div>
-			<ul class="wp-block-govpack-profile__comms-icons govpack-inline-list">
-				<?php
-				foreach ( $links as &$link ) {
-
-					if ( ! gp_icon_exists( $link['slug'] ) ) {
-						continue;
-					}
-					
-					$classes = [
-						'wp-block-govpack-profile__contact',
-						'wp-block-govpack-profile__contact--hide-label',
-						"wp-block-govpack-profile__contact--{$link['slug']}",
-					];
-					$classes = join( ' ', $classes );
-
-					?>
-					<li class="<?php echo esc_attr( $classes ); ?>">
-						<a href="<?php echo esc_url( $link['href'] ); ?>" title="Link to <?php echo esc_attr( $link['text'] ); ?>">
-							<span class="wp-block-govpack-profile__contact__icon wp-block-govpack-profile__contact__icon--<?php echo esc_attr( $link['slug'] ); ?>">
-								<?php echo esc_svg( gp_get_icon( $link['slug'] ) ); ?>
-							</span>
-							<span class="wp-block-govpack-profile__contact__label">
-								<?php echo esc_html( $link['text'] ); ?>
-							</span>
-						</a>
-					</li>
-				<?php } ?>
-			</ul>
-		</div>
-		<?php
-		return ob_get_clean();
-	}
-}
-
-
-if ( ! function_exists( 'gp_should_show_link' ) ) {
-	function gp_should_show_link( $key, $attributes ) {
-		if ( ! isset( $attributes['showOtherLinks'] ) ) {
-			return false;
-		}
-
-		if (
-			( isset( $attributes['selectedLinks'] ) ) &&
-			( isset( $attributes['selectedLinks'][ $key ] ) ) &&
-			( $attributes['selectedLinks'][ $key ] === false )
-		) {
-			return false;
-		}
-
-		return true;
-	}
-}
-
-/**
- * Utility Function that Outputs a Profiles's Social Media Sections
- * 
- * @param array $profile_data Data about the profile.
- * @param array $attributes Attributes from the Block.
- */
-/*
-function gp_social_media( $profile_data, $attributes ) {
-
-		$template = '<div class="wp-block-govpack-profile__social">
-			<ul class="wp-block-govpack-profile__services govpack-vertical-list">
-			%s
-			</ul>
-			</div>';
-
-		$content = '';
-
-		if ( $attributes['selectedSocial']['showOfficial'] && isset( $profile_data['social']['official'] ) ) {
-			$content .= gp_social_media_row( 'Official', $profile_data['social']['official'] );
-		}
-
-		if ( $attributes['selectedSocial']['showCampaign'] && isset( $profile_data['social']['campaign'] ) ) {
-			$content .= gp_social_media_row( 'Campaign', $profile_data['social']['campaign'] );
-		}
-
-		if ( $attributes['selectedSocial']['showPersonal'] && isset( $profile_data['social']['personal'] ) ) {
-			$content .= gp_social_media_row( 'Personal', $profile_data['social']['personal'] );
-		}
-
-		return sprintf( $template, $content ); 
-	}
-}
-*/
-
-if ( ! function_exists( 'gp_get_icons' ) ) {
-	function gp_get_icons() {
+if(!function_exists("gp_get_icons")){
+	function gp_get_icons(){
 		return gp()->icons()->all();
 	}
 }
@@ -467,58 +213,7 @@ if ( ! function_exists( 'gp_has_icon' ) ) {
 		return gp()->icons()->exists( $key );
 	}
 }
-/**
- * Utility Function that Outputs a Profiles's Social Media Row
- * 
- * @param string $label Row label to shoe.
- * @param array  $links Links for social media profiles.
- */
-/*
-function gp_social_media_row( $label, $links = [] ) {
 
-
-		$outer_template = 
-			'<li class="wp-block-govpack-profile__social_group">
-				<div class="wp-block-govpack-profile__label">%s: </div>
-				<ul class="govpack-inline-list">
-					%s
-				</ul>
-			</li>';
-
-		$content = '';
-
-		$services = [ 'facebook', 'x', 'linkedin', 'instagram', 'youtube' ];
-
-		foreach ( $services as $service ) {
-			if ( ! isset( $links[ $service ] ) || ! $links[ $service ] ) {
-				continue;
-			}
-
-			$classes = [
-				'wp-block-govpack-profile__contact',
-				'wp-block-govpack-profile__contact--hide-label',
-				"wp-block-govpack-profile__contact--{$service}",
-			];
-
-			$classes = join( ' ', $classes );
-
-			$icon         = '<span class="wp-block-govpack-profile__contact__icon wp-block-govpack-profile__contact__icon--{%s}">%s</span>';
-			$contact_icon = sprintf( $icon, $service, gp_get_icon( $service ) );
-
-			$content .=  
-			"<li class=\"{$classes} \">
-				<a href=\"{$links[$service]}\" class=\"wp-block-govpack-profile__contact__link\">
-					{$contact_icon}
-					<span class=\"wp-block-govpack-profile__contact__label\">{$service}</span>
-				</a>
-			</li>";
-
-		}
-
-		return sprintf( $outer_template, $label, $content ); 
-	}
-}
-*/
 
 /**
  * Utility Function that Outputs a Profiles's Contact Sections
--- includes/govpack-functions.php
+++ includes/govpack-functions.php
@@ -58,3 +58,10 @@ if ( ! function_exists( 'gp_doing_it_wrong' ) ) {
 		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
+
+if(!function_exists("gp_deprecated")){
+	function gp_deprecated(string $function_name, string $version) : void {
+		$message = sprintf("The function <code>%s</code> has been deprecated from version <code>%s</code> and will be removed in an upcoming release.", $function, $version);
+		gp_doing_it_wrong( $function_name, $message, $version );
+	}
+}
