cbc46dd844654e548ce578e041b120535327f900
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-06-17 12:11:51 +0100

    resolve merge conflicts

--- includes/govpack-functions-template.php
+++ includes/govpack-functions-template.php
@@ -328,12 +328,12 @@ if(!function_exists("gp_get_photo_styles")){
 			"height" => (isset($attributes['avatarSize']) ? esc_attr($attributes['avatarSize'] . "px") : false)
 		);
 
-		return gp_style_attr_generator($rules);
+		return gp_style_attribute_generator($rules);
 	}
 }
 
-if(!function_exists("gp_style_attr_generator")){
-	function gp_style_attr_generator($rules){
+if(!function_exists("gp_style_attribute_generator")){
+	function gp_style_attribute_generator($rules){
 		// filter the rules where the getter returns false;
 		$rules = array_filter($rules);
 
