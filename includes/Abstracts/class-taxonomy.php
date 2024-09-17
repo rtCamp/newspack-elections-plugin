93c25da63631125d0ec23910382300e097a0e331
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-09-17 12:14:27 +0100

    removed the taxonomy seeding function

--- includes/Abstracts/class-taxonomy.php
+++ includes/Abstracts/class-taxonomy.php
@@ -29,38 +29,5 @@ abstract class Taxonomy {
 		return [ Profile::CPT_SLUG ];
 	}
 
-	/**
-	 * Seed taxonomies with default data.
-	 */
-	public static function seed() {
-		$file_path = GOVPACK_PLUGIN_DIR . 'assets/json/' . static::SLUG . '.json';
 
-		if ( ! file_exists( $file_path ) || 0 !== validate_file( $file_path ) ) {
-			return;
-		}
-
-		$json = file_get_contents( $file_path ); // phpcs:ignore WordPressVIPMinimum.Performance.FetchingRemoteData.FileGetContentsUnknown
-		$data = json_decode( $json );
-
-		if ( json_last_error() !== JSON_ERROR_NONE ) {
-			return;
-		}
-
-		$count = 0;
-		foreach ( $data as $item ) {
-			$term_exists_result = term_exists( $item, static::TAX_SLUG ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.term_exists_term_exists
-
-			if ( is_array( $term_exists_result ) ) {
-				continue;
-			}
-
-			$result = wp_insert_term( $item, static::TAX_SLUG );
-
-			if ( ! is_wp_error( $result ) ) {
-				++$count;
-			}
-		}
-
-		return $count;
-	}
 }
