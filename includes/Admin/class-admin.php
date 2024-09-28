d7237f52bf458e5c2349edc714eb44ae020099ec
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-09-28 14:28:20 +0100

    changed the icon over, cannot work out why there is a grey tinge

--- includes/Admin/class-admin.php
+++ includes/Admin/class-admin.php
@@ -92,13 +92,19 @@ class Admin {
 	}
 
 	public function get_menu_svg() {
-		return '<svg width="153" height="213" viewBox="0 0 153 213" xmlns="http://www.w3.org/2000/svg">
-	<path fill="rgba(240, 246, 252, 0.6)" clip-rule="evenodd" d="M68.6875 148.556C18.0258 148.556 0.4375 110.963 0.4375 76.2125C0.4375 26.4625 37.5625 0.400024 76.8125 0.400024C90.6717 0.400024 107.693 3.21878 116.562 10.5563V4.55628H152.563V156.556C152.563 166 139.438 212.713 76.5625 212.713C11.4332 212.713 4.5625 163.963 4.5625 160.556H48.5625C48.5625 165.9 59.6875 174.338 77.4375 174.338C98.1875 174.338 110.562 158.088 110.562 146.556V130.556C110.562 130.556 96.38 148.556 68.6875 148.556ZM82.4425 54.9375L63.255 24.6875L60.4425 62.9375L24.4425 72.9375L58.4425 86.9375V124.938C62.4884 120.213 83.6925 95.5 83.6925 95.5L118.442 108.938C118.442 108.938 103.546 86.0825 98.4425 78.9375V74.9375C98.0675 74.9375 120.442 46.9375 120.442 46.9375L82.4425 54.9375Z"/>
-</svg>';
+		return '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M4 16H20M4 16V18.5C4 19.3284 4.67157 20 5.5 20H18.5C19.3284 20 20 19.3284 20 18.5V16M4 16V10.5C4 9.67157 4.67157 9 5.5 9H8M20 16V10.5C20 9.67157 19.3284 9 18.5 9H16" stroke="white" stroke-width="1.5" stroke-linejoin="round"/>
+<path d="M16 9H18.5C19.3284 9 20 9.67157 20 10.5V14.5C20 15.3284 19.3284 16 18.5 16H5.5C4.67157 16 4 15.3284 4 14.5V10.5C4 9.67157 4.67157 9 5.5 9H8" stroke="white" stroke-width="1.5" stroke-linejoin="round"/>
+<path d="M7 12.5H17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M16 12V6C16 5.17157 15.3284 4.5 14.5 4.5H9.5C8.67157 4.5 8 5.17157 8 6V12" stroke="white" stroke-width="1.5"/>
+<path d="M10.5 8.5L11.5 9.5L13.5 7.5" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+';
 	}
 
 	public function create_menu_svg() {
-		return sprintf( 'data:image/svg+xml;base64,%s', base64_encode( $this->get_menu_svg() ) );
+		return sprintf( 'data:image/svg+xml;base64,%s', base64_encode($this->get_menu_svg()));
+		//return sprintf( 'data:image/svg+xml;base64,%s', "PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTQgMTZIMjBNNCAxNlYxOC41QzQgMTkuMzI4NCA0LjY3MTU3IDIwIDUuNSAyMEgxOC41QzE5LjMyODQgMjAgMjAgMTkuMzI4NCAyMCAxOC41VjE2TTQgMTZWMTAuNUM0IDkuNjcxNTcgNC42NzE1NyA5IDUuNSA5SDhNMjAgMTZWMTAuNUMyMCA5LjY3MTU3IDE5LjMyODQgOSAxOC41IDlIMTYiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+CjxwYXRoIGQ9Ik0xNiA5SDE4LjVDMTkuMzI4NCA5IDIwIDkuNjcxNTcgMjAgMTAuNVYxNC41QzIwIDE1LjMyODQgMTkuMzI4NCAxNiAxOC41IDE2SDUuNUM0LjY3MTU3IDE2IDQgMTUuMzI4NCA0IDE0LjVWMTAuNUM0IDkuNjcxNTcgNC42NzE1NyA5IDUuNSA5SDgiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+CjxwYXRoIGQ9Ik03IDEyLjVIMTciIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPHBhdGggZD0iTTE2IDEyVjZDMTYgNS4xNzE1NyAxNS4zMjg0IDQuNSAxNC41IDQuNUg5LjVDOC42NzE1NyA0LjUgOCA1LjE3MTU3IDggNlYxMiIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSIxLjUiLz4KPHBhdGggZD0iTTEwLjUgOC41TDExLjUgOS41TDEzLjUgNy41IiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjEuNSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+Cjwvc3ZnPgo=" );
 	}
 	/**
 	 * Creates the Govpack Menu in the Dashboard Navigation
