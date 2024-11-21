7cb4d56e409d9c4ef0ee3e93a79c1e69168aa3b2
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2024-11-21 13:21:14 +0000

    add way to show and hide the group appender

--- src/blocks/ProfileRowGroup/edit.js
+++ src/blocks/ProfileRowGroup/edit.js
@@ -2,11 +2,33 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useBlockProps, useInnerBlocksProps} from "@wordpress/block-editor"
+import { useBlockProps, useInnerBlocksProps, InnerBlocks, store as blockEditorStore} from "@wordpress/block-editor"
+import {useSelect} from "@wordpress/data";
 
-function Edit( {attributes, setAttributes, context, ...props} ) {
+import "./edit.scss"
+
+const Appender = (props) => {
 	const blockProps = useBlockProps();
-	const innerBlockProps = useInnerBlocksProps(blockProps);
+	console.log("appender", blockProps)
+	return (
+		<InnerBlocks.ButtonBlockAppender />
+	)
+}
+
+function Edit( {attributes, setAttributes, context, clientId, ...props} ) {
+
+	const { isBlockSelected, hasSelectedInnerBlock } = useSelect( (select) => {
+		return {
+			isBlockSelected : select(blockEditorStore).isBlockSelected(clientId),
+			hasSelectedInnerBlock : select(blockEditorStore).hasSelectedInnerBlock(clientId),
+		}
+	});
+
+	console.log("isBlockSelected", isBlockSelected)
+	const blockProps = useBlockProps();
+	const innerBlockProps = useInnerBlocksProps(blockProps, {
+		renderAppender : (isBlockSelected || hasSelectedInnerBlock) ? InnerBlocks.ButtonBlockAppender : undefined
+	});
 	
 
     return (
