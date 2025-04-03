1134e81ddea18285793d9daa1b5622c8350670a9
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2025-04-03 17:37:42 +0100

    Removed lots of old panel implementation

--- src/editor/meta/Panels/index.jsx
+++ src/editor/meta/Panels/index.jsx
@@ -1,11 +1,5 @@
-
-
-export { CommunicationsPanel } from "./Communications";
-export { SocialPanel } from "./Social";
 export { MetadataIdsPanel } from "./Meta";
-export { AboutPanel } from "./About";
-export { OfficePanel } from "./Office";
-export { PositionPanel } from "./Position";
+
 
 
 
