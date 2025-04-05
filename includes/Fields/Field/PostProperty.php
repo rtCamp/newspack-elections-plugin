7c4ad195a9498700ef6718ff97a445f1181bd8e2
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2025-04-05 17:39:10 +0100

    post_excerpt and post_title now return the rendered and raw fields like the normal rest api

--- includes/Fields/Field/PostProperty.php
+++ includes/Fields/Field/PostProperty.php
@@ -47,23 +47,63 @@ class PostProperty extends \Govpack\Fields\Field {
 			return 'incompatible source';
 		}
 
-		$value = trim( $model->post->{$this->source_key} );
-
 		if ( \method_exists( $this, $this->source_key ) ) {
-			return call_user_func( [ $this, $this->source_key ], $value, $model );
+			return call_user_func( [ $this, $this->source_key ], $model );
 		}
 
-		return $value;
+		return trim( $model->post->{$this->source_key} );
 	}
 
-	public function post_excerpt( $value, $model ) {
-		$value = apply_filters( 'get_the_excerpt', $value, $model->post );
-		$value = apply_filters( 'the_excerpt', $value );
-		return $value;
+	public function post_excerpt( $model ) {
+
+		$excerpt = trim( $model->post->{$this->source_key} );
+		$excerpt = apply_filters( 'get_the_excerpt', $excerpt, $model->post );
+		$excerpt = apply_filters( 'the_excerpt', $excerpt );
+
+		return $excerpt;
 	}
 
-	public function post_title( $value, $model ) {
+	public function post_title( $model ) {
+		$value = trim( $model->post->{$this->source_key} );
 		$value = get_the_title( $model->post );
+		return $value;
+	}
+
+
+
+	public function value_for_rest( Profile $model ) {
+
+		$value = $this->raw( $model );
+
+		if($this->source_key === "post_excerpt"){
+
+			$data = array(
+				'raw'       => $value,
+				'rendered'  => post_password_required( $model->post ) ? '' : $value,
+				'protected' => (bool) $model->post->post_password,
+			);
+
+			return $data;
+		}
+
+		if($this->source_key === "post_title"){
+
+			$data = array(
+				'raw'       => $value,
+				'protected' => (bool) $model->post->post_password,
+			);
+
+			add_filter( 'protected_title_format', array( $this, 'protected_title_format' ) );
+			add_filter( 'private_title_format', array( $this, 'protected_title_format' ) );
+
+			$data['rendered'] = get_the_title( $model->post->ID );
+
+			remove_filter( 'protected_title_format', array( $this, 'protected_title_format' ) );
+			remove_filter( 'private_title_format', array( $this, 'protected_title_format' ) );
+
+			return $data;
+		}
+		
 		return $value;
 	}
 }
