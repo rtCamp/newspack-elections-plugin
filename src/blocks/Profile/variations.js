12a4f37136f5887fb249551238d043b865222fae
Stewart Ritchie <stewart@poweredbycoffee.co.uk>
2025-09-05 18:48:53 +0100

    small tweaks

--- src/blocks/Profile/variations.js
+++ src/blocks/Profile/variations.js
@@ -3,18 +3,216 @@ import {justifyLeft, layout, mediaAndText} from "@wordpress/icons"
 import { DEFAULT_TEMPLATE, FULL_TEMPLATE, HORIZONTAL_TEMPLATE } from "./edit/default-template"
 
 
+const defaultProfile = {
+	'name' : "profile-default",
+	'title' : "Profile (No Tint)",
+	'description' : "Profile",
+	isDefault : true,
+	icon : layout,
+	isActive: [ 'variation', 'className' ],
+	attributes : {
+		"variation" : false
+	},
+}
+
+const miniProfile = {
+	'name' : "profile-mini",
+	'title' : "Mini Profile (No Tint)",
+	isDefault : false,
+	isActive: [ 'variation', 'className' ],
+	icon : justifyLeft,
+	example : {
+		attributes : {
+			"variation": "mini",
+			"preview" : true,
+			"width" : "wide",
+			"align": "none",
+			"backgroundColor":"accent",
+			"verticalAlignment":"center",
+			"fontSize":"medium",
+			"textColor":"base-2",
+			"style":{
+				"spacing":{
+					"padding":{
+						"top":"0.5rem","bottom":"0.5rem","left":"0.5rem","right":"0.5rem"}
+				},
+				"typography":{ "textAlign":"left" }
+			},
+			"layout":{
+				"type":"flex",
+				"verticalAlignment":"middle",
+				"justifyContent":"left",
+				"flexWrap":"nowrap"
+			},
+		},
+		innerBlocks : [
+			{
+				name : "core/post-featured-image", 
+				attributes :{"style":{"layout":{"selfStretch":"fixed","flexSize":"130px"},"border":{"radius":"100px"}}}
+			},
+			{
+				name : "core/group", 
+				attributes : {"style":{"spacing":{"blockGap":"0.2rem"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} , 
+				innerBlocks: [
+					{
+						name : "npe/profile-name", 
+						attributes : {"style":{"typography":{"fontStyle":"normal","fontWeight":"1000","fontSize":"1.4rem"},"spacing":{"padding":{"bottom":"0.5rem"}}}}, 
+						innerBlocks : []
+					},
+					{
+						name :"npe/profile-row", 
+						attributes :{
+							"field": {
+								"key": "legislative_body", 
+								"type":"taxonomy"
+							},
+							"fontSize":"small"
+						}, 
+						innerBlocks : []
+					},
+					{
+						name :"npe/profile-row", 
+						attributes :{
+							"field": {
+								"key": "party",
+								"type":"taxonomy"
+							},
+							"fontSize":"small"
+						}, 
+						innerBlocks :[]
+					},
+					{
+						name :"npe/profile-row", 
+						attributes :{
+							"field": {
+								"key": "officeholder_status",
+								"type":"taxonomy"
+							},
+							"fontSize":"small"
+						}, 
+						innerBlocks :[]
+					}
+				]
+			}
+		]
+	},
+	attributes : {
+		"variation": "mini",
+		"width" : "wide",
+		"align": "none",
+		
+		"verticalAlignment":"center",
+		"fontSize":"small",
+		
+		"style":{
+			"spacing":{
+				"padding":{
+					"top":"0.5rem","bottom":"0.5rem","left":"0.5rem","right":"0.5rem"}
+			},
+			"typography":{"textAlign":"left"},
+			
+		},
+		"layout":{
+			"type":"flex",
+			"verticalAlignment":"middle",
+			"justifyContent":"left",
+			"flexWrap":"nowrap"
+		},
+		
+	},
+	innerBlocks : [
+		[
+			"core/post-featured-image", 
+			{"style":{"layout":{"selfStretch":"fixed","flexSize":"130px"},"border":{"radius":"100px"}}}
+		],
+		[
+			"npe/profile-row-group",
+			{"style":{"spacing":{"blockGap":"0.2rem"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} , 
+			[
+				["npe/profile-name", {"style":{"typography":{"fontStyle":"normal","fontWeight":"1000","fontSize":"1.4rem"},"spacing":{"padding":{"bottom":"0.5rem"}}}}, []],
+				["npe/profile-row", { 
+					"field":{
+						"type":"taxonomy",
+						"key":"legislative_body",
+					},
+					"fontSize":"small" 
+				}, []],
+				["npe/profile-row", { 
+					"field":{
+						"type":"taxonomy",
+						"key":"party"
+					},
+					"fontSize":"small"
+				}, []],
+				["npe/profile-row", { 
+					"field":{
+						"type":"taxonomy",
+						"key":"status"
+					},
+					"fontSize":"small"
+				}, []]
+			]
+		]
+	]
+}
+
+
+
+const fullProfile = {
+	'name' : "profile-full",
+	'title' : "Full Profile",
+	'description' : "Profile",
+	isDefault : false,
+	icon : layout,
+	isActive: [ 'variation', 'className' ],
+	attributes : {
+		"variation" : "full",
+		"style":{"spacing":{"blockGap":"0"}}
+	},
+	"innerBlocks": FULL_TEMPLATE
+}
 
 export const variations = [
+	defaultProfile,
 	{
-		'name' : "gp-profile-full",
-		'title' : "Full Profile",
-		'description' : "Profile",
-		isDefault : false,
-		icon : layout,
-		attributes : {
-			"style":{"spacing":{"blockGap":"0"}}
-		},
-		"innerBlocks": FULL_TEMPLATE
+		...defaultProfile,
+		name : "profile-default-blue-tint",
+		title : "Profile (Blue Tint)",
+		attributes: {
+			...defaultProfile.attributes,
+			className: "is-style-blue-tint"
+		}
+	},
+	{
+		...defaultProfile,
+		name : "profile-default-red-tint",
+		title : "Profile (Red Tint)",
+		attributes: {
+			...defaultProfile.attributes,
+			className: "is-style-red-tint"
+		}
+	},
+	{
+		...defaultProfile
+	},
+	fullProfile,
+	{
+		...fullProfile,
+		name : "profile-full-blue-tint",
+		title : "Full Profile (Blue Tint)",
+		attributes: {
+			...fullProfile.attributes,
+			className: "is-style-blue-tint"
+		}
+	},
+	{
+		...fullProfile,
+		name : "profile-full-red-tint",
+		title : "Full Profile (Red Tint)",
+		attributes: {
+			...fullProfile.attributes,
+			className: "is-style-red-tint"
+		}
 	},
 	{
 		'name' : "gp-profile-full-inline",
@@ -97,142 +295,24 @@ export const variations = [
 		]
 			
 	},
+	miniProfile,
 	{
-		'name' : "gp-profile-mini",
-		'title' : "Mini Profile",
-		isDefault : false,
-		icon : justifyLeft,
-		example : {
-			attributes : {
-				"preview" : true,
-				"width" : "wide",
-				"align": "none",
-				"backgroundColor":"accent",
-				"verticalAlignment":"center",
-				"fontSize":"medium",
-				"textColor":"base-2",
-				"style":{
-					"spacing":{
-						"padding":{
-							"top":"0.5rem","bottom":"0.5rem","left":"0.5rem","right":"0.5rem"}
-					},
-					"typography":{ "textAlign":"left" }
-				},
-				"layout":{
-					"type":"flex",
-					"verticalAlignment":"middle",
-					"justifyContent":"left",
-					"flexWrap":"nowrap"
-				},
-			},
-			innerBlocks : [
-				{
-					name : "core/post-featured-image", 
-					attributes :{"style":{"layout":{"selfStretch":"fixed","flexSize":"130px"},"border":{"radius":"100px"}}}
-				},
-				{
-					name : "core/group", 
-					attributes : {"style":{"spacing":{"blockGap":"0.2rem"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} , 
-					innerBlocks: [
-						{
-							name : "npe/profile-name", 
-							attributes : {"style":{"typography":{"fontStyle":"normal","fontWeight":"1000","fontSize":"1.4rem"},"spacing":{"padding":{"bottom":"0.5rem"}}}}, 
-							innerBlocks : []
-						},
-						{
-							name :"npe/profile-row", 
-							attributes :{
-								"field": {
-									"key": "legislative_body", 
-									"type":"taxonomy"
-								},
-								"fontSize":"small"
-							}, 
-							innerBlocks : []
-						},
-						{
-							name :"npe/profile-row", 
-							attributes :{
-								"field": {
-									"key": "party",
-									"type":"taxonomy"
-								},
-								"fontSize":"small"
-							}, 
-							innerBlocks :[]
-						},
-						{
-							name :"npe/profile-row", 
-							attributes :{
-								"field": {
-									"key": "officeholder_status",
-									"type":"taxonomy"
-								},
-								"fontSize":"small"
-							}, 
-							innerBlocks :[]
-						}
-					]
-				}
-			]
-		},
-		attributes : {
-			"width" : "wide",
-			"align": "none",
-			
-			"verticalAlignment":"center",
-			"fontSize":"small",
-			
-			"style":{
-				"spacing":{
-					"padding":{
-						"top":"0.5rem","bottom":"0.5rem","left":"0.5rem","right":"0.5rem"}
-				},
-				"typography":{"textAlign":"left"},
-				
-			},
-			"layout":{
-				"type":"flex",
-				"verticalAlignment":"middle",
-				"justifyContent":"left",
-				"flexWrap":"nowrap"
-			},
-			
-		},
-		innerBlocks : [
-			[
-				"core/post-featured-image", 
-				{"style":{"layout":{"selfStretch":"fixed","flexSize":"130px"},"border":{"radius":"100px"}}}
-			],
-			[
-				"npe/profile-row-group",
-				{"style":{"spacing":{"blockGap":"0.2rem"},"layout":{"selfStretch":"fill","flexSize":null}},"layout":{"type":"constrained"}} , 
-				[
-					["npe/profile-name", {"style":{"typography":{"fontStyle":"normal","fontWeight":"1000","fontSize":"1.4rem"},"spacing":{"padding":{"bottom":"0.5rem"}}}}, []],
-					["npe/profile-row", { 
-						"field":{
-							"type":"taxonomy",
-							"key":"legislative_body",
-						},
-						"fontSize":"small" 
-					}, []],
-					["npe/profile-row", { 
-						"field":{
-							"type":"taxonomy",
-							"key":"party"
-						},
-						"fontSize":"small"
-					}, []],
-					["npe/profile-row", { 
-						"field":{
-							"type":"taxonomy",
-							"key":"status"
-						},
-						"fontSize":"small"
-					}, []]
-				]
-			]
-		]
+		...miniProfile,
+		name : "profile-mini-blue-tint",
+		title : "Mini Profile (Blue Tint)",
+		attributes: {
+			...miniProfile.attributes,
+			className: "is-style-blue-tint"
+		}
+	},
+	{
+		...miniProfile,
+		name : "profile-mini-red-tint",
+		title : "Mini Profile (Red Tint)",
+		attributes: {
+			...miniProfile.attributes,
+			className: "is-style-red-tint"
+		}
 	},
 	{
 		name : "horizontal-profile",
